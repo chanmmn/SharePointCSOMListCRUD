902a01134e0cfe37541a65a71f9316d1248716b3
chanmmn <33271651+chanmmn@users.noreply.github.com>
2024-06-17 05:00:04 -0700

    Commit ConAppSPRevivist
    Commit ConAppSPRevivist

---
+++ 2024/ConAppSPRevivist/ConAppSPRevivist/Program.cs
@@ -0,0 +1,63 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+using SP = Microsoft.SharePoint.Client;
+
+namespace ConAppSPRevivist
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            GetSharePointItem();
+
+        }
+
+        private static void GetSharePointItem()
+        {
+            //youe url must be correct
+            string siteUrlSource = "https://someone.sharepoint.com/DevSite/";
+
+            //this is where you use your declared url
+            ClientContext clientContextSource = new ClientContext(siteUrlSource);
+
+            //For SharePoint Online this is the important section
+            string password = "password";
+
+            SecureString securePassword = new SecureString();
+            foreach (char c in password)
+            {
+                securePassword.AppendChar(c);
+            }
+
+            clientContextSource.Credentials = new SharePointOnlineCredentials("someone@someone.onmicrosoft.com", securePassword);
+
+            //the List name goes here
+            SP.List oList = clientContextSource.Web.Lists.GetByTitle("testList");
+
+
+            CamlQuery camlQuery = new CamlQuery();
+            camlQuery.ViewXml = "<View><RowLimit> 100</RowLimit></View>";
+
+            ListItemCollection collListItem = oList.GetItems(camlQuery);
+            clientContextSource.Load(collListItem,
+                 items => items.Include(
+                     item => item.Id));
+
+
+            //if your url is wrong or list name is wrong then you will normal get exception here
+
+            clientContextSource.ExecuteQuery();
+            foreach (ListItem oListItem in collListItem)
+            {
+
+                Console.WriteLine("ID: {0}", oListItem.Id);
+            }
+        }
+    }
+}
